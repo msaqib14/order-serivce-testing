cbfef5d1d51476d4f0ad85620b86260ed8d0d89a
Mohd Saqib <116734915+msaqib14@users.noreply.github.com>
2025-02-04 14:39:46 +0530

    project skeleton added

---
+++ order-service-testing/src/main/java/com/order/order_service_testing/OrderServiceTestingApplication.java
@@ -0,0 +1,13 @@
+package com.order.order_service_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceTestingApplication.class, args);
+	}
+
+}
---
+++ order-service-testing/src/test/java/com/order/order_service_testing/OrderServiceTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.order_service_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
