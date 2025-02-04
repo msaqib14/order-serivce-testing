8f822cb431e058d67ccc820f29440e17c40c6f69
Mohd Saqib <116734915+msaqib14@users.noreply.github.com>
2025-02-04 14:59:03 +0530

    I have created 1 api for getting all order i.e; getAllOrder

---
+++ order-service-testing/src/main/java/com/order/order_service_testing/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.order.order_service_testing.repository;
+
+import com.order.order_service_testing.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
+
+
+}
